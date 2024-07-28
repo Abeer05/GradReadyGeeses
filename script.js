1693d0bfbe462e0bd449e070bd3996bde135a677
Abeer Malik <125997236+Abeer05@users.noreply.github.com>
2024-07-28 13:49:22 -0400

    Final

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("input").addEventListener("change", function () {
+  document.getElementById("main").classList.add("hidden");
+  document.getElementById("congrats").classList.remove("hidden");
+});
